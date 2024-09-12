42ff1ecfe1bc48eea2abfea1cdd27a55c91710c1
Yves John B. Tapispisan <bancolitayves@gmail.com>
2024-09-12 22:28:11 +0800

    fix routing issues

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\StudentRecordController;
-use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\RBACController;
 use App\Http\Middleware\CheckToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Login
-Route::get('/login', [LoginController::class, 'login'])->name('web.login');
-
-// Student Records Routes
-Route::get('/records', [StudentRecordController::class, 'index']);
-Route::post('/records', [StudentRecordController::class, 'store']);
-Route::get('/records/{id}', [StudentRecordController::class, 'show']);
-Route::delete('/records/{id}', [StudentRecordController::class, 'destroy']);
-Route::put('/records/{id}', [StudentRecordController::class, 'update']);
-Route::get('/deleted-records', [StudentRecordController::class, 'getDeletedRecords']);
-Route::post('/records/retrieve/{id}', [StudentRecordController::class, 'retrieveDeletedRecord']);
-Route::delete('/records/permanently-delete/{id}', [StudentRecordController::class, 'deletePermanently']);
-
 // Appointment Routes
 Route::middleware([CheckToken::class])->group(function () {
-    Route::get('/appointments', [AppointmentController::class, 'index']);
-    Route::post('/appointments', [AppointmentController::class, 'store']);
-    Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy']);
-    Route::post('/appointments/{id}/accept', [AppointmentController::class, 'accept']);
-});
+    Route::get('/login', function () {
+        return view ('welcome');
+    });
 
-// Role-Based Access Control (RBAC) Routes
-Route::middleware([CheckToken::class])->group(function () {
-    Route::post('/roles', [RBACController::class, 'storeRole']);
-    Route::post('/assign-role/{userId}', [RBACController::class, 'assignRole']);
+    Route::get('/appointments', function () {
+        return view ('welcome');
+    });
+
+    Route::get('/student-records', function () {
+        return view ('welcome');
+    });
+
+    Route::get('/review-applicants', function () {
+        return view ('welcome');
+    });
+
+    Route::get('/create-record', function () {
+        return view ('welcome');
+    });
+
+    Route::get('/history', function () {
+        return view ('welcome');
+    });
+
+    Route::get('/application-status', function () {
+        return view ('welcome');
+    });
 });
